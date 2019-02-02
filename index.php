735519e09f690bc091d94c8b30ee90a0ba0a2ffd
rahmatdanizaki <rahmatdanizaki@yahoo.com>
2019-02-02 11:52:49 +0700

    Perubahan pertama

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<form action="proses.php" method="post" target="blank">
+<textarea name="v" id="nmapscan" cols="100" rows="10"></textarea>
+<input type="submit" value="kirim">
+</form>
+</body>
+</html>
